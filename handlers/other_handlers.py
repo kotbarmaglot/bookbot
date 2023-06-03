6e7ec35a77fb2420074f3696b8ec93e2a0848a17
newage <allonewage@gmail.com>
2023-06-03 16:09:15 +0300

    finish book bot

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,11 +1,11 @@
 from aiogram import Router
 from aiogram.types import Message
-from lexicon.lexicon_ru import LEXICON_RU
 
-# Инициализируем роутер уровня модуля
 router: Router = Router()
 
 
+# Этот хэндлер будет реагировать на любые сообщения пользователя,
+# не предусмотренные логикой работы бота
 @router.message()
-async def send_answer(message: Message):
-    await message.answer(text=LEXICON_RU['other_answer'])
+async def send_echo(message: Message):
+    await message.answer(f'Это эхо! {message.text}')
--- services/file_handling.py
+++ services/file_handling.py
@@ -1,27 +1,66 @@
-import random
+import os
+import re
 
-from lexicon.lexicon_ru import LEXICON_RU
+BOOK_PATH = '/home/newage/projects-wsl/bookbot/book/book.txt'
+PAGE_SIZE = 1050
 
+book: dict[int, str] = {}
 
-def get_bot_choice() -> str:
-    return random.choice(['rock', 'paper', 'scissors'])
-
-
-def _normalize_user_answer(user_answer: str) -> str:
-    for key in LEXICON_RU:
-        if LEXICON_RU[key] == user_answer:
-            return key
-    raise Exception
+# Функция, возвращающая строку с текстом страницы и ее размер
 
 
-def get_winner(user_choice: str, bot_choice: str) -> str:
-    user_choice = _normalize_user_answer(user_choice)
-    rules: dict[str, str] = {'rock': 'scissors',
-                             'scissors': 'paper',
-                             'paper': 'rock'}
-    if user_choice == bot_choice:
-        return 'nobody_won'
-    elif rules[user_choice] == bot_choice:
-        return 'user_won'
-    else:
-        return 'bot_won'
+# МОЯ РЕАЛИЗАЦИЯ
+# def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
+#     end = [',', '.', '!', ':', ';', '?']
+
+#     len_out = min(size + start, len(text))
+
+#     if text[len_out-1] in end and text[len_out-2] not in end:
+#         return text[start:len_out], len_out-start
+
+#     if text[len_out-1] in end and (text[len_out-2] in end or text[len_out] in end):
+#         while text[len_out-1] in end and (text[len_out-2] in end or text[len_out] in end):
+#             len_out = len_out - 1
+
+#         if text[len_out-1] not in end:
+#             while text[len_out-1] not in end:
+#                 len_out = len_out - 1
+#         return text[start:len_out], len_out-start
+
+#     if text[len_out-1] not in end:
+#         while text[len_out-1] not in end:
+#             len_out = len_out - 1
+
+#         return text[start:len_out], len_out-start
+
+
+def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
+    edit_text = re.sub(r'[.,!?:;]\.+$', '', text[start:start+size])
+    edit_text = re.findall(r'(?s).+[.,!?:;]', edit_text)
+    return *edit_text, len(*edit_text)
+
+
+# Не удаляйте эти объекты - просто используйте
+PAGE_SIZE = 1050
+
+
+# Дополните эту функцию, согласно условию задачи
+def prepare_book(path: str) -> None:
+    with open(path, "r") as f:
+        text = f.read()
+
+        x = 0
+        i = 1
+        y = 0
+
+        while x < len(text):
+            text_out, y = _get_part_text(text, x, PAGE_SIZE)
+            text_out = re.sub(r'[\r\t\0]', '', text_out)
+            text_out = re.sub(r'^\s+', '', text_out)
+            x = x + y
+            book[i] = text_out
+            i = i + 1
+
+
+# Вызов функции prepare_book для подготовки книги из текстового файла
+prepare_book(os.path.join(os.getcwd(), BOOK_PATH))
